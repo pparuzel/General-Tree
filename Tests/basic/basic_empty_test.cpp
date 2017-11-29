d94ee1b045841ab16e8908a086e8f3994a43d4d9
Pawel Paruzel <pawelparuzel95@gmail.com>
2017-11-29 01:22:59 +0100

    Add first tests

---
+++ Tests/basic/basic_empty_test.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+#include "../../GeneralTree/GeneralTree.hpp"
+
+
+// General Tests
+TEST(BasicTests, EmptyTreeTests) {
+    rcn::GeneralTree<int> empty_tree{};
+
+    ASSERT_EQ(empty_tree.root(), rcn::GeneralTree<int>::iterator{});
+    ASSERT_EQ(empty_tree.getNumberOfChildren(empty_tree.root()), 0);
+    ASSERT_EQ(empty_tree.size(), 0);
+    ASSERT_EQ(empty_tree.root(), empty_tree.end());
+    ASSERT_NO_THROW(empty_tree.clear());
+}
